38dde064940893ea0b9d9c0a21edf3ba728a0f7c
Alangkarn Srisamutr <Alangkarn.Sri@roojai.com>
2021-01-12 09:44:35 +0700

    section 45 : Serving up static Assests

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,10 @@
 const path = require('path')
 const express = require('express')
 
-console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
-
 const app = express()
 const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
-
-app.get('/help', (req,res) => {
-    res.send({
-        name: 'Alancx',
-        age: 24
-    })
-})
-
-
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About page</h1>')
-})
 
 app.get('/weather', (req, res) => {
     res.send({
